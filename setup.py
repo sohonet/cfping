988ae1843e5be8a00596ad6fc18c8fe407e7244e
davidc0le <david.cole@sohonet.com>
2021-03-24 15:33:31 +0000

    Rev version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "cfping",
-    version = "0.3.0",
+    version = "0.3.1",
     description = "Test the performance and availability of the Rackspace cloudfiles or Openstack swift service.",
     author = "Clay McClure",
     author_email = "clay@daemons.net",
